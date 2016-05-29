2047379945583e0edba590cf44faf5925b3d5606
James Abley <james.abley@gmail.com>
2016-05-29 16:05:14 +0100

    Fix formatting
    That will teach me to write Java outside of an IDE.

--- src/main/java/uk/co/eternus/byteconversion/MyBenchmark.java
+++ src/main/java/uk/co/eternus/byteconversion/MyBenchmark.java
@@ -51,55 +51,52 @@ public class MyBenchmark {
         StringBuilder buf = new StringBuilder();
 
         try (InputStream is = MyBenchmark.class.getResourceAsStream("/test.json")) {
-        	 BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        	
-        	String line;
-        	while ((line = reader.readLine()) != null) {
-            	buf.append(line);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buf.append(line);
             }
             reader.close();
-	    	
-	    	final InputStream stream = new ByteArrayInputStream(buf.toString().getBytes());
-		 	reader = new BufferedReader(new InputStreamReader(stream));
 
-			copy(reader, new NullWriter());
-    	} catch (IOException ignore) {}
+            final InputStream stream = new ByteArrayInputStream(buf.toString().getBytes());
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+            copy(reader, new NullWriter());
+        } catch (IOException ignore) {}
 
     }
 
-    private void copy(Reader in, Writer out) throws IOException {
-    	char[] buffer = new char[1024];
-		int len = in.read(buffer);
-		while (len != -1) {
-    		out.write(buffer, 0, len);
-    		len = in.read(buffer);
-		}
-		in.close();
-		out.close();
+    private void copy(Reader in , Writer out) throws IOException {
+        char[] buffer = new char[1024];
+        int len = in .read(buffer);
+        while (len != -1) {
+            out.write(buffer, 0, len);
+            len = in .read(buffer);
+        } in .close();
+        out.close();
     }
 
     @Benchmark
     public void withoutNeedlessConversions() {
-		try (InputStream is = MyBenchmark.class.getResourceAsStream("/test.json")) {
-		final BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        try (InputStream is = MyBenchmark.class.getResourceAsStream("/test.json")) {
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
-		copy(reader, new NullWriter());
+            copy(reader, new NullWriter());
 
         } catch (IOException ignore) {}
     }
 
     private static class NullWriter extends Writer {
 
-    	@Override
-    	public void close() throws IOException {}
+        @Override
+        public void close() throws IOException {}
 
-    	@Override
-    	public void flush() throws IOException {}
+        @Override
+        public void flush() throws IOException {}
 
-  		@Override
-  		public void write(char[] cbuf, int off, int len) {}
-  		
-	}
+        @Override
+        public void write(char[] cbuf, int off, int len) {}
+
+    }
 }
-
-
